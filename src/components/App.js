42dba5e12a5dd99d5cc72fac4e1df309c9af3684
George Papagapitos <george.g.papagapitos@gmail.com>
2021-03-13 17:59:14 -0600

    more material ui styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import { useState } from 'react';
 import './App.css';
+import SideMenu from './SideMenu';
+import Header from './Header';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
@@ -23,127 +27,145 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
+// const useStyles = makeStyles({
+//   root: {
+//     background: 'linear-gradient(45deg, #333, red)',
+//     border: 0,
+//     marginBottom: 15,
+//     borderRadius: 15,
+//     color: 'white',
+//     padding: '5px 30px'
+//   }
+// });
+
+// const theme = createMuiTheme({
+//   typography: {
+//     h2: {
+//       fontSize: 36,
+//       marginBottom: 15,
+//     }
+//   },
+//   palette: {
+//     primary: {
+//       main: green[400],
+//     },
+//     secondary: {
+//       main: orange[400]
+//     }
+//   }
+// });
+
+// function ButtonStyled() {
+//   const classes = useStyles();
+//   return <Button className={classes.root}>Test Styled Button</Button>
+// }
+
+// const CheckboxExample = () => {
+//   const [checked, setChecked] = useState(true);
+//   return (
+//     <FormControlLabel 
+//       control={
+//         <Checkbox
+//           checked={checked}
+//           icon={<SaveIcon />}
+//           checkedIcon={<SaveIcon />}
+//           onChange={(e) => setChecked(e.target.checked)}
+//           inputProps={{
+//             'aria-label': 'secondary checkbox'
+//           }}
+//         />}
+//         label='Testing Checkbox'
+//     />
+//   )
+// }
+
 const useStyles = makeStyles({
-  root: {
-    background: 'linear-gradient(45deg, #333, red)',
-    border: 0,
-    marginBottom: 15,
-    borderRadius: 15,
-    color: 'white',
-    padding: '5px 30px'
+  appMain: {
+    paddingLeft: '320px',
+    width:'100%'
   }
-});
-
-const theme = createMuiTheme({
-  typography: {
-    h2: {
-      fontSize: 36,
-      marginBottom: 15,
-    }
-  },
-  palette: {
-    primary: {
-      main: green[400],
-    },
-    secondary: {
-      main: orange[400]
-    }
-  }
-});
-
-function ButtonStyled() {
-  const classes = useStyles();
-  return <Button className={classes.root}>Test Styled Button</Button>
-}
-
-const CheckboxExample = () => {
-  const [checked, setChecked] = useState(true);
-  return (
-    <FormControlLabel 
-      control={
-        <Checkbox
-          checked={checked}
-          icon={<SaveIcon />}
-          checkedIcon={<SaveIcon />}
-          onChange={(e) => setChecked(e.target.checked)}
-          inputProps={{
-            'aria-label': 'secondary checkbox'
-          }}
-        />}
-        label='Testing Checkbox'
-    />
-  )
-}
+})
 
 const App = () => {
+
+  const classes = useStyles();
+
   return (
-    <ThemeProvider theme={theme}>
+    <>
+      <SideMenu />
+      <div className={classes.appMain}>
+        <Header />
+      </div>
+      <CssBaseline />
+    </>
 
-      <Container maxWidth="sm">
+    // <ThemeProvider theme={theme}>
 
-        <div className="App">
-          <AppBar color="secondary">
-            <Toolbar>
-              <IconButton>
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6">
-                MUI TEST THEME
-              </Typography>
-              <Button>
-                Login
-              </Button>
-            </Toolbar>
-          </AppBar>
+    //   <Container maxWidth="sm">
 
-          <Typography variant="h2" component="div">
-            Test Test Test
-          </Typography>
+    //     <div className="App">
+    //       <AppBar color="secondary">
+    //         <Toolbar>
+    //           <IconButton>
+    //             <MenuIcon />
+    //           </IconButton>
+    //           <Typography variant="h6">
+    //             MUI TEST THEME
+    //           </Typography>
+    //           <Button>
+    //             Login
+    //           </Button>
+    //         </Toolbar>
+    //       </AppBar>
 
-          <Typography variant="subtitle1">
-            Test Test Test
-          </Typography>
+    //       <Typography variant="h2" component="div">
+    //         Test Test Test
+    //       </Typography>
 
-          <ButtonStyled />
+    //       <Typography variant="subtitle1">
+    //         Test Test Test
+    //       </Typography>
 
-          <Grid container spacing={2} justify="center">
-            <Grid item xs={3} sm={6}>
-              <Paper style={{
-                height: 75, width: '100%' }} />
-            </Grid>
-            <Grid item xs={3} sm={6}>
-              <Paper style={{
-                height: 75, width: '100%' }} />
-            </Grid>
-            <Grid item xs={3} sm={6}>
-              <Paper style={{
-                height: 75, width: '100%' }} />
-            </Grid>
-          </Grid>
+    //       <ButtonStyled />
 
-          <TextField 
-            variant="filled"
-            color="secondary"
-            type="email"
-            label="Your Email"
-            placeholder="testing@test.com"
-          />
-          <CheckboxExample />
-          <ButtonGroup variant="contained">
-            <Button
-              startIcon={<SaveIcon />}
-              color="primary">
-              Save
-            </Button>
-            <Button
-              startIcon={<DeleteIcon />}
-              color="secondary">
-              Delete
-            </Button>        
-          </ButtonGroup>
-        </div>
-      </Container>
-    </ThemeProvider>
+    //       <Grid container spacing={2} justify="center">
+    //         <Grid item xs={3} sm={6}>
+    //           <Paper style={{
+    //             height: 75, width: '100%' }} />
+    //         </Grid>
+    //         <Grid item xs={3} sm={6}>
+    //           <Paper style={{
+    //             height: 75, width: '100%' }} />
+    //         </Grid>
+    //         <Grid item xs={3} sm={6}>
+    //           <Paper style={{
+    //             height: 75, width: '100%' }} />
+    //         </Grid>
+    //       </Grid>
+
+    //       <TextField 
+    //         variant="filled"
+    //         color="secondary"
+    //         type="email"
+    //         label="Your Email"
+    //         placeholder="testing@test.com"
+    //       />
+    //       <CheckboxExample />
+    //       <ButtonGroup variant="contained">
+    //         <Button
+    //           startIcon={<SaveIcon />}
+    //           color="primary">
+    //           Save
+    //         </Button>
+    //         <Button
+    //           startIcon={<DeleteIcon />}
+    //           color="secondary">
+    //           Delete
+    //         </Button>        
+    //       </ButtonGroup>
+    //     </div>
+    //   </Container>
+    // </ThemeProvider>
   );
 }
 
---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,38 @@
+import { makeStyles, withStyles } from '@material-ui/core';
+
+// const useStyles = makeStyles({
+//   sideMenu: {
+//     display: 'flex',
+//     flexDirection: 'column',
+//     position: 'absolute',
+//     left: '0px',
+//     width: '320px',
+//     height: '100%',
+//     backgroundColor: '#253053'
+//   }
+// });
+
+const style = {
+  sideMenu: {
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'absolute',
+    left: '0px',
+    width: '320px',
+    height: '100%',
+    backgroundColor: '#253053'
+  }
+}
+
+const SideMenu = (props) => {
+
+  const { classes } = props;
+
+  return (
+    <div className={classes.sideMenu}>
+
+    </div>
+  )
+}
+
+export default withStyles(style)(SideMenu);
